6b49b90f1cff0857df15700c0a6b87da3e014a78
NITHISHguviii <nithishkumarv0612@gmail.com>
2024-07-28 18:01:33 +0530

    error resolved

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,31 +1,34 @@
 // axiosInstance.js
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000/api/" : "https://pinterest-backend-1wai.onrender.com/api", 
-  timeout: 1000,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/api/"
+      : "https://pinterest-backend-1wai.onrender.com/api",
+  timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('token')}` 
-  }
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
 
 axiosInstance.interceptors.request.use(
-  config => {
-    console.log('Request Interceptor:', config);
+  (config) => {
+    console.log("Request Interceptor:", config);
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
 
 axiosInstance.interceptors.response.use(
-  response => {
-    console.log('Response Interceptor:', response);
+  (response) => {
+    console.log("Response Interceptor:", response);
     return response;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
